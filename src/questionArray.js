3ab78241688324b1a427543a103c722dd92c44a6
KennedyB1 <1994_simon@live.se>
2022-12-28 20:52:49 +0100

    Fix

--- src/questionArray.js
+++ src/questionArray.js
@@ -1,7 +1,56 @@
+/*
+
 const exampleCards = [
   'Spades Ace',
   'Spades 2',
   'Spades 3',
 ];
 
-export default exampleCards;
+export default exampleCards; */
+
+const questions = [{
+  questionText: '1',
+  answerOptions: [
+    '1',
+    '2',
+    '3',
+    '4'
+  ],
+  correctAnswer: '4',
+  answerInfo: 'Det här går bra',
+},
+{
+  questionText: '2',
+  answerOptions: [
+    '1',
+    '2',
+    '3',
+    '4'
+  ],
+  correctAnswer: '4',
+  answerInfo: 'Det här går bra',
+},
+{
+  questionText: '3',
+  answerOptions: [
+    '1',
+    '2',
+    '3',
+          '4'
+  ],
+  correctAnswer: '4',
+  answerInfo: 'Det här går bra',
+},
+{
+  questionText: '4',
+  answerOptions: [
+    '1',
+    '2',
+    '3',
+          '4'
+  ],
+  correctAnswer: '4',
+  answerInfo: 'Det här går bra',
+}
+
+];
